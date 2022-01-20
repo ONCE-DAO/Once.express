c7a70a291d61462328ab43cb9ccdeb0ba4efb293
phibar <philippbartels@outlook.com>
2022-01-21 00:27:39 +0100

    OOP once

--- src/2_systems/OnceExpress.ts
+++ src/2_systems/OnceExpress.ts
@@ -1,15 +1,16 @@
 import { IOR } from '../../../once.ts@main/src/3_services/IOR.interface'
 import { Server } from '../../../once.ts@main/src/3_services/Server.interface'
-import { Once } from './../../../once.ts@main/src/3_services/Once.interface'
+import { Once } from '../../../once.ts@main/src/2_systems/Once.class'
 
 import express from 'express'
 import http from 'http'
 import cors from 'cors'
 import serveIndex from 'serve-index'
 import path from 'path'
+import { OnceMode, OnceState } from '../../../once.ts@main/src/3_services/Once.interface'
 
-export class OnceExpress implements Once {
-  protected express = express()
+export class OnceExpress extends Once {
+  private express = express()
 
   id: string | undefined
   name: string | undefined
@@ -23,12 +24,22 @@ export class OnceExpress implements Once {
       res.sendFile(path.resolve('dist/EAMD.ucp/Components/tla/EAM/once.ts@main/src/5_ux/view/html/Once.html'))
     })
     instance.express.use('/', serveIndex('dist', { icons: true }))
-    
+
     instance.express.use('/', express.static('dist', {}))
     // instance.express.use('/docs', express.static('docs', {}))
 
     await instance.startServer(port)
+    instance.state = OnceState.STARTED
     return instance
+  }
+
+  /**
+   *
+   */
+  constructor () {
+    super()
+    this.mode = OnceMode.NODE_JS
+    global.ONCE && this.onces.push(global.ONCE)
   }
 
   async discover (): Promise<IOR[]> {
